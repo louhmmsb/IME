101a4c10b0db1b74202e953c7d70d40af545d019
Lourenço <louhmmsb@hotmail.com>
2020-05-16 10:11:28 -0300

    Fiz usn ex desafios

--- 3sem/desafios/aula19/b.cpp
+++ 3sem/desafios/aula19/b.cpp
@@ -3,25 +3,40 @@ using namespace std;
 
 #define si(x) scanf("%d", &x);
 #define pi(x) printf("%d\n", x);
-#define pb push_back;
+#define pb(x) push_back(x);
 
 typedef long long int ll;
 typedef unsigned long long int ull;
 
-int dp[31][241], d, sumTime, mins[31], maxs[31];
-
-int pd(int dat, int sumat){
-    if(sumat == sumTime);
-    if(dat == d);
-
-    int& at = dp[dat][sumat];
-    if(at != -1) return at;
-
-    at =  
-}
-
 int main(){
+    int d, sumTime, sumMin = 0, sumMax = 0;
+    vector<int> mins, maxs, resp;
     si(d);si(sumTime);
 
-    for(int i=0; i<d; i++) si(mins[i]), si(max[i]);
+    for(int i=0; i<d; i++){
+        int auxMin, auxMax;
+        si(auxMin);si(auxMax);
+        mins.pb(auxMin);
+        resp.pb(auxMin);
+        maxs.pb(auxMax);
+        sumMin+=auxMin;
+        sumMax+=auxMax;
+    }
+    if(sumTime>=sumMin && sumTime<=sumMax){
+        int dif = sumTime-sumMin;
+        cout<<"YES\n";
+        int i=0;
+        while(dif>0){
+            if(resp[i]<maxs[i]){
+                resp[i]++;
+                dif--;
+            }
+            else i++;
+        }
+        for(auto a : resp) cout<<a<<" ";cout<<"\n";
+    }
+    else cout<<"NO\n";
+
+    return 0;
+
 }
